fc84cdf0ba125e51de8ce1d98ae14326b40e3f33
swolfenden9 <wolfendenbuild@gmail.com>
2023-02-22 15:46:56 +1000

    fixes cypress error

--- cypress/e2e/basic.cy.ts
+++ cypress/e2e/basic.cy.ts
@@ -2,8 +2,4 @@ describe('Basic specs', () => {
   beforeEach(() => {
     cy.visit('/')
   })
-
-  it('Check for jokes link', () => {
-    cy.get('.jokes-link').should('have.attr', 'href').and('include', 'api/joke')
-  })
 })
